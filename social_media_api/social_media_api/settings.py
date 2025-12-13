de22570d0f7bd9af056dc9ecff7552840223eb97
Titus Simiyu <muchangatitus@gmail.com>
2025-12-13 14:22:54 +0300

    Implement Likes and Notifications functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = []
 
 
-# Application definition "SECURE_BROWSER_XSS_FILTER", "X_FRAME_OPTIONS", "SECURE_SSL_REDIRECT"
+# Application definition "SECURE_BROWSER_XSS_FILTER", "PORT" "X_FRAME_OPTIONS", "SECURE_SSL_REDIRECT"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
