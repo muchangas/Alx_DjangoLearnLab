6d996eb4e4a8ea9f50f46991d58a3762d9a77ef4
Titus Simiyu <muchangatitus@gmail.com>
2025-12-13 12:59:08 +0300

    feat: Initial setup of Social Media API with custom user model and DRF token authentication.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,4 +1,4 @@
-# accounts/serializers.py
+# ["from rest_framework.authtoken.models import Token", "serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user"]accounts/serializers.py
 
 from rest_framework import serializers
 from .models import CustomUser
